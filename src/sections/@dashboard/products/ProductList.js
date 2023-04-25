c7689ccf5dbae0daf0e7fc82fce38d2efc7fac2b
amalthomas-exe <thomasamal479@gmail.com>
2023-04-25 13:49:13 +0530

    Updayes

--- src/sections/@dashboard/products/ProductList.js
+++ src/sections/@dashboard/products/ProductList.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 // @mui
 import { Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
 import ShopProductCard from './ProductCard';
 
 // ----------------------------------------------------------------------
@@ -13,9 +14,12 @@ export default function ProductList({ products, ...other }) {
   return (
     <Grid container spacing={3} {...other}>
       {products.map((product) => (
-        <Grid key={product.id} item xs={12} sm={6} md={3}>
-          <ShopProductCard product={product} />
-        </Grid>
+          <Grid key={product.id} item xs={12} sm={6} md={3}>
+            <Link to='../user'>
+            <ShopProductCard product={product} />
+            </Link>
+            
+          </Grid>
       ))}
     </Grid>
   );
