c82cc33ab1a58905cda42b070dd341ce2e3f1930
AlwinJacobThomas <kailath.ajt@gmail.com>
2023-04-26 13:00:49 +0530

    styled registration

--- src/pages/RegistrationPage1.js
+++ src/pages/RegistrationPage1.js
@@ -28,6 +28,12 @@ function RegistrationPage1() {
     ]);
   };
 
+  const handleRemoveStudent = (index) => {
+    const list = [...students];
+    list.splice(index, 1);
+    setStudents(list);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const url = `/event/${id}/`;
@@ -43,96 +49,102 @@ function RegistrationPage1() {
   return (
     <div className="table-container">
       <table>
-        <tr>
-          <th>Name</th>
-          <th>KTU Id</th>
-          <th>College</th>
-          <th>Gender</th>
-          <th>Phone</th>
-          <th>Accommodation</th>
-        </tr>
-
-        {students.map((student, index) => (
-          <tr key={index}>
-            <td htmlFor={`name-${index}`}>
-              <input
-                type="text"
-                id={`name-${index}`}
-                name="name"
-                value={student.name}
-                onChange={(e) => handleInputChange(e, index)}
-                required
-              />
-            </td>
-            <td htmlFor={`ktuId-${index}`}>
-              <input
-                type="text"
-                id={`ktu_id-${index}`}
-                name="ktu_id"
-                value={student.ktu_id}
-                onChange={(e) => handleInputChange(e, index)}
-                required
-              />
-            </td>
-            <td htmlFor={`college-${index}`}>
-              <input
-                type="text"
-                id={`college-${index}`}
-                name="college"
-                value={student.college}
-                onChange={(e) => handleInputChange(e, index)}
-                required
-              />
-            </td>
-            <td htmlFor={`gender-${index}`}>
-              <select
-                id={`gender-${index}`}
-                name="gender"
-                value={student.gender}
-                onChange={(e) => handleInputChange(e, index)}
-                required
-              >
-                <option value="">Select gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-            </td>
-            <td htmlFor={`phone-${index}`}>
-              <input
-                type="tel"
-                id={`phone-${index}`}
-                name="phone"
-                value={student.phone}
-                onChange={(e) => handleInputChange(e, index)}
-                pattern="[0-9]{10}"
-                required
-              />
-            </td>
-            <td>
-              <label htmlFor={`day1-${index}`}>
-                May 3
-                <input
-                  type="checkbox"
-                  id={`day1-${index}`}
-                  name="accommodation1"
-                  checked={student.accommodation1}
-                  onChange={(e) => handleInputChange(e, index)}
-                />
-              </label>
-              <label htmlFor={`day2-${index}`}>
-                May 4
-                <input
-                  type="checkbox"
-                  id={`day2-${index}`}
-                  name="accommodation2"
-                  checked={student.accommodation2}
-                  onChange={(e) => handleInputChange(e, index)}
-                />
-              </label>
-            </td>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>KTU Id</th>
+            <th>College</th>
+            <th>Gender</th>
+            <th>Phone</th>
+            <th>Accommodation</th>
+            <th> </th>
           </tr>
-        ))}
+
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td htmlFor={`name-${index}`}>
+                <input
+                  type="text"
+                  id={`name-${index}`}
+                  name="name"
+                  value={student.name}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                />
+              </td>
+              <td htmlFor={`ktuId-${index}`}>
+                <input
+                  type="text"
+                  id={`ktu_id-${index}`}
+                  name="ktu_id"
+                  value={student.ktu_id}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                />
+              </td>
+              <td htmlFor={`college-${index}`}>
+                <input
+                  type="text"
+                  id={`college-${index}`}
+                  name="college"
+                  value={student.college}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                />
+              </td>
+              <td htmlFor={`gender-${index}`}>
+                <select
+                  id={`gender-${index}`}
+                  name="gender"
+                  value={student.gender}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                >
+                  <option value="">Select gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </td>
+              <td htmlFor={`phone-${index}`}>
+                <input
+                  type="tel"
+                  id={`phone-${index}`}
+                  name="phone"
+                  value={student.phone}
+                  onChange={(e) => handleInputChange(e, index)}
+                  pattern="[0-9]{10}"
+                  required
+                />
+              </td>
+              <td>
+                <label htmlFor={`day1-${index}`}>
+                  <input
+                    type="checkbox"
+                    id={`day1-${index}`}
+                    name="accommodation1"
+                    checked={student.accommodation1}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />&nbsp;May 3
+                </label>
+                <label htmlFor={`day2-${index}`}>
+                  <input
+                    type="checkbox"
+                    id={`day2-${index}`}
+                    name="accommodation2"
+                    checked={student.accommodation2}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />&nbsp;May 4
+                </label>
+              </td>
+              <td>
+                <button onClick={handleRemoveStudent} className="btn-danger">
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
       <button type="button" onClick={handleAddStudent} className="btn-outline">
         Add Team Member
