b018d202a110302d6c1a8a5cf4602bdf66930a3a
Amal Thomas <thomasamal479@gmail.com>
2023-04-26 11:06:54 +0530

    Dashboard ready

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -51,19 +51,19 @@ export default function DashboardAppPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
+            <AppWidgetSummary title="Colleges" total={9}  />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
+            <AppWidgetSummary title="Registrations" total={0} color="info"  />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
+            <AppWidgetSummary title="Days" total={2} color="warning" />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
+            <AppWidgetSummary title="Events" total={"90 +"} color="error"  />
           </Grid>
         </Grid>
       </Container>
