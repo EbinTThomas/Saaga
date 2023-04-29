ae92072a237425891ee5a89da119fe2444a022ff
AlwinJacobThomas <kailath.ajt@gmail.com>
2023-04-29 11:27:29 +0530

    working ExpoPage resolved merge conflicts latest

--- src/pages/ExpoPage.js
+++ src/pages/ExpoPage.js
@@ -1,7 +1,6 @@
 import { React, useState } from 'react';
 import '../styles/RegistrationPage.css';
 import axios from '../services/api/axios';
-import { ProductList } from '../sections/@dashboard/products';
 
 const EXPO_URL = '/event/expo-reg/';
 
@@ -18,29 +17,55 @@ function RegistrationPage() {
 
   const [successMsg, setSuccessMsg] = useState('');
   const [errMsg, setErrMsg] = useState('');
-  // const axiosPrivate = useAxiosPrivate()
 
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
-
-  const handleCloseFilter = () => {
-    setOpenFilter(false);
-  };
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const isAuthenticated = localStorage.getItem('isAuthenticated');
-    console.log(isAuthenticated)
-    if (isAuthenticated === 'false') {
-      navigate(
-        '/login',
-        { replace: true },
-      )
+  const handleProjectInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'department') {
+      if (value === 'Other') {
+        setOtherDepartment(true);
+        setDept('')
+      } else {
+        setOtherDepartment(false);
+        setDept(value)
+      }
     }
-  }, [])
+    else if (name === 'otherDepartment') {
+      setDept(value);
+    }
+    else {
+      setProject({ ...project, [name]: value });
+    }
+  };
 
+  const handleInputChange = (e, index) => {
+    const { name, value, type, checked } = e.target;
+    const list = [...students];
+    if (type === 'checkbox') {
+      list[index][name] = checked;
+    } else {
+      list[index][name] = value;
+    }
+    setStudents(list);
+    if (successMsg) {
+      setSuccessMsg('');
+    }
+    if (errMsg) {
+      setErrMsg('');
+    }
+  };
+
+  const handleAddStudent = () => {
+    setStudents([
+      ...students,
+      { name: '', ktu_id: '', gender: '', phone: '', accommodation1: false, accommodation2: false },
+    ]);
+  };
+
+  const handleRemoveStudent = (index) => {
+    const list = [...students];
+    list.splice(index, 1);
+    setStudents(list);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -164,8 +189,93 @@ function RegistrationPage() {
             <th> </th>
           </tr>
 
-        <ProductList products={events} />
-      </Container>
-    </>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td htmlFor={`name-${index}`}>
+                <input
+                  type="text"
+                  id={`name-${index}`}
+                  name="name"
+                  value={student.name}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                />
+              </td>
+              <td htmlFor={`ktuId-${index}`}>
+                <input
+                  type="text"
+                  id={`ktu_id-${index}`}
+                  name="ktu_id"
+                  value={student.ktu_id}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                />
+              </td>
+              <td htmlFor={`gender-${index}`}>
+                <select
+                  id={`gender-${index}`}
+                  name="gender"
+                  value={student.gender}
+                  onChange={(e) => handleInputChange(e, index)}
+                  required
+                >
+                  <option value="">Select gender</option>
+                  <option value="MALE">Male</option>
+                  <option value="FEMALE">Female</option>
+                  <option value="OTHER">Other</option>
+                </select>
+              </td>
+              <td htmlFor={`phone-${index}`}>
+                <input
+                  type="tel"
+                  id={`phone-${index}`}
+                  name="phone"
+                  value={student.phone}
+                  onChange={(e) => handleInputChange(e, index)}
+                  pattern="[0-9]{10}"
+                  required
+                />
+              </td>
+              <td>
+                <label htmlFor={`day1-${index}`}>
+                  <input
+                    type="checkbox"
+                    id={`day1-${index}`}
+                    name="accommodation1"
+                    checked={student.accommodation1}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
+                  &nbsp;May 3
+                </label>
+                <label htmlFor={`day2-${index}`}>
+                  <input
+                    type="checkbox"
+                    id={`day2-${index}`}
+                    name="accommodation2"
+                    checked={student.accommodation2}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
+                  &nbsp;May 4
+                </label>
+              </td>
+              <td>
+                <button onClick={(e) => handleRemoveStudent(index)} className="btn-danger">
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button type="button" onClick={handleAddStudent} className="btn-outline">
+        Add Team Member
+      </button>
+      <br />
+      <button type="submit" className="btn-submit" onClick={handleSubmit}>
+        Register
+      </button>
+    </div>
   );
 }
+
+export default RegistrationPage;
